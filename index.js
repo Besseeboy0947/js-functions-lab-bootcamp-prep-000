4bf17bc6d6315e7a09e791900ce8c5993bd9fb1f
Besseeboy0947 Bessee <ikaquanbessee@yahoo.com>
2017-10-24 11:39:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,3 +9,6 @@ return "Happy holidays, you!"
 function happyHolidayTo(holiday,name) {
   return `Happy ${holiday}, ${name}!`
 }  
+function holidayCountdown(holiday,days) {
+ return "it's 20 days till mothers day"
+}
